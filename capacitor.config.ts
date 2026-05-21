60cd6107f3456a0523b8e79564b8304cff1be5e8
BBRC777 <contact@mrfstudios.com>
2026-05-21 01:32:30 -0700

    chore: add capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mrfstudios.learnpath',
+  appName: 'learnpath',
+  webDir: 'public'
+};
+
+export default config;
